e41891e788c90808dbd8fc160f009f6ece7fdff6
zhaoruiwen <1121427818@qq.com>
2025-12-11 11:38:16 +0800

    pre commit

--- src/main/java/com/yt/server/service/HandleWasteTimeService.java
+++ src/main/java/com/yt/server/service/HandleWasteTimeService.java
@@ -5,10 +5,7 @@ import com.yt.server.entity.*;
 import com.yt.server.mapper.TraceFieldMetaMapper;
 import com.yt.server.mapper.TraceTableRelatedInfoMapper;
 import com.yt.server.mapper.TraceTimestampStatisticsMapper;
-import com.yt.server.util.BaseUtils;
-import com.yt.server.util.ConnectionManager;
-import com.yt.server.util.IoUtil;
-import com.yt.server.util.VarConst;
+import com.yt.server.util.*;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
@@ -25,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -153,11 +151,52 @@ public class HandleWasteTimeService {
 //                            String finalVarName = varName;
                             List<UniPoint> singleVarDataList = uniPointList.stream().filter(item -> varName.equals(item.getVarName())).toList();
                             List<UniPoint> originalFilterVarDataList = new CopyOnWriteArrayList<>(singleVarDataList);
-                            int bucketSize = originalFilterVarDataList.size() / downSamplingRate;
-                            String downsamplingTableName = parentDownsamplingTableName.concat("_").concat(varName).concat("_").concat(String.valueOf(downSamplingRate));
-                            if (bucketSize > 0) {
-                                singleVarDataList = LTThreeBuckets.sorted(originalFilterVarDataList, bucketSize);
+                            // 如果数据点太少，直接跳过，不做任何降采样
+                            if (originalFilterVarDataList.size() <= downSamplingRate) {
+                                continue;
                             }
+                            // ====================== 逻辑修改开始 ======================
+                            BigDecimal volatilityIndex = BigDecimal.ZERO;
+                            if (originalFilterVarDataList.size() > 1) {
+                               BigDecimal minY = originalFilterVarDataList.get(0).getY();
+                                BigDecimal maxY = originalFilterVarDataList.get(0).getY();
+                                BigDecimal totalVerticalDistance = BigDecimal.ZERO;
+
+                                for (int i = 1; i < originalFilterVarDataList.size(); i++) {
+                                    UniPoint currentPoint = originalFilterVarDataList.get(i);
+                                    UniPoint previousPoint = originalFilterVarDataList.get(i - 1);
+                                    BigDecimal currentY = currentPoint.getY();
+
+                                    if (currentY.compareTo(minY) < 0) {
+                                        minY = currentY;
+                                    }
+                                    if (currentY.compareTo(maxY) > 0) {
+                                        maxY = currentY;
+                                    }
+                                    totalVerticalDistance = totalVerticalDistance.add(currentY.subtract(previousPoint.getY()).abs());
+                                }
+                                BigDecimal verticalRange = maxY.subtract(minY);
+                                if (verticalRange.compareTo(BigDecimal.ZERO) > 0) {
+                                    volatilityIndex = totalVerticalDistance.divide(verticalRange, 2, RoundingMode.HALF_UP);
+                                }
+                            }
+
+                            int bucketSize = originalFilterVarDataList.size() / downSamplingRate;
+                            final BigDecimal THRESHOLD = new BigDecimal("2");
+
+                            if (volatilityIndex.compareTo(THRESHOLD) > 0) {
+                                logger.info("【降采样】变量 '{}', 折腾指数: {}. 使用 Min-Max 算法.", varName, volatilityIndex);
+                                singleVarDataList = MinMaxDownsampler.downsample(originalFilterVarDataList, bucketSize * 2);
+                            } else {
+                                logger.info("【降采样】变量 '{}', 折腾指数: {}. 使用 LTTB 算法.", varName, volatilityIndex);
+                                if (bucketSize > 0) {
+                                    singleVarDataList = LTThreeBuckets.sorted(originalFilterVarDataList, bucketSize);
+                                } else {
+                                    singleVarDataList = originalFilterVarDataList;
+                                }
+                            }
+                            // ======================= 逻辑修改结束 =======================
+                            String downsamplingTableName = parentDownsamplingTableName.concat("_").concat(varName).concat("_").concat(String.valueOf(downSamplingRate));
                             //save to database
                             List<Object[]> dataObjArr = convertPojoList2ObjListArr(singleVarDataList, 2);
                             BaseUtils.executeDownsamplingBatchUpdate(connection, downsamplingTableName, dataObjArr);
@@ -214,30 +253,74 @@ public class HandleWasteTimeService {
         if (CollectionUtils.isNotEmpty(uniPointList)) {
             for (String varName : varNames) {
                 List<UniPoint> singleVarDataList = uniPointList.stream().filter(item -> varName.equals(item.getVarName())).toList();
-                List<UniPoint> originalFilterVarDataList = new CopyOnWriteArrayList<>(singleVarDataList);
-                int bucketSize = originalFilterVarDataList.size() / downSamplingRate;
-                String downsamplingTableName = parentDownsamplingTableName.concat("_").concat(varName).concat("_").concat(String.valueOf(downSamplingRate));
-                if (bucketSize > 0) {
-                    singleVarDataList = LTThreeBuckets.sorted(originalFilterVarDataList, bucketSize);
+                if (CollectionUtils.isNotEmpty(singleVarDataList)) {
+                    List<UniPoint> originalFilterVarDataList = new CopyOnWriteArrayList<>(singleVarDataList);
+                    // 如果数据点太少，直接跳过，不做任何降采样
+                    if (originalFilterVarDataList.size() <= downSamplingRate) {
+                        continue;
+                    }
+                    // ====================== 逻辑修改开始 ======================
+                    BigDecimal volatilityIndex = BigDecimal.ZERO;
+                    if (originalFilterVarDataList.size() > 1) {
+                        BigDecimal minY = originalFilterVarDataList.get(0).getY();
+                        BigDecimal maxY = originalFilterVarDataList.get(0).getY();
+                        BigDecimal totalVerticalDistance = BigDecimal.ZERO;
+
+                        for (int i = 1; i < originalFilterVarDataList.size(); i++) {
+                            UniPoint currentPoint = originalFilterVarDataList.get(i);
+                            UniPoint previousPoint = originalFilterVarDataList.get(i - 1);
+                            BigDecimal currentY = currentPoint.getY();
+
+                            if (currentY.compareTo(minY) < 0) {
+                                minY = currentY;
+                            }
+                            if (currentY.compareTo(maxY) > 0) {
+                                maxY = currentY;
+                            }
+                            totalVerticalDistance = totalVerticalDistance.add(currentY.subtract(previousPoint.getY()).abs());
+                        }
+                        BigDecimal verticalRange = maxY.subtract(minY);
+                        if (verticalRange.compareTo(BigDecimal.ZERO) > 0) {
+                            volatilityIndex = totalVerticalDistance.divide(verticalRange, 2, RoundingMode.HALF_UP);
+                        }
+                    }
+
+                    int bucketSize = originalFilterVarDataList.size() / downSamplingRate;
+                    final BigDecimal THRESHOLD = new BigDecimal("2");
+
+                    if (volatilityIndex.compareTo(THRESHOLD) > 0) {
+                        logger.info("【降采样】变量 '{}', 折腾指数: {}. 使用 Min-Max 算法.", varName, volatilityIndex);
+                        singleVarDataList = com.yt.server.util.MinMaxDownsampler.downsample(originalFilterVarDataList, bucketSize * 2);
+                    } else {
+                        logger.info("【降采样】变量 '{}', 折腾指数: {}. 使用 LTTB 算法.", varName, volatilityIndex);
+                        if (bucketSize > 0) {
+                            singleVarDataList = LTThreeBuckets.sorted(originalFilterVarDataList, bucketSize);
+                        } else {
+                            singleVarDataList = originalFilterVarDataList;
+                        }
+                    }
+                    // ======================= 逻辑修改结束 =======================
+                    String downsamplingTableName = parentDownsamplingTableName.concat("_").concat(varName).concat("_").concat(String.valueOf(downSamplingRate));
+
+                    //save to database
+                    List<Object[]> dataObjArr = convertPojoList2ObjListArr(singleVarDataList, 2);
+                    BaseUtils.executeDownsamplingBatchUpdate(connection, downsamplingTableName, dataObjArr);
+                    Pair<List<UniPoint>, Integer> secondDownsamplingPair = handleBigDownsampling(singleVarDataList, varName, 4, connection, downSamplingRate, parentDownsamplingTableName);
+                    Pair<List<UniPoint>, Integer> thirdDownsamplingPair = handleBigDownsampling(secondDownsamplingPair.getLeft(), varName, 4, connection, secondDownsamplingPair.getRight(), parentDownsamplingTableName);
+                    handleBigDownsampling(thirdDownsamplingPair.getLeft(), varName, 2, connection, thirdDownsamplingPair.getRight(), parentDownsamplingTableName);
+                    handleBigDownsampling(thirdDownsamplingPair.getLeft(), varName, 4, connection, thirdDownsamplingPair.getRight(), parentDownsamplingTableName);
                 }
-                //save to database
-                List<Object[]> dataObjArr = convertPojoList2ObjListArr(singleVarDataList, 2);
-                BaseUtils.executeDownsamplingBatchUpdate(connection, downsamplingTableName, dataObjArr);
-                Pair<List<UniPoint>, Integer> secondDownsamplingPair = handleBigDownsampling(singleVarDataList, varName, 4, connection, downSamplingRate, parentDownsamplingTableName);
-                Pair<List<UniPoint>, Integer> thirdDownsamplingPair = handleBigDownsampling(secondDownsamplingPair.getLeft(), varName, 4, connection, secondDownsamplingPair.getRight(), parentDownsamplingTableName);
-                handleBigDownsampling(thirdDownsamplingPair.getLeft(), varName, 2, connection, thirdDownsamplingPair.getRight(), parentDownsamplingTableName);
-                handleBigDownsampling(thirdDownsamplingPair.getLeft(), varName, 4, connection, thirdDownsamplingPair.getRight(), parentDownsamplingTableName);
+                traceTimestampStatistics.setTempTimestamp(lastMaxTimestamp);
+                traceTimestampStatistics.setLastEndTimestamp(currentMaxTimestamp);
+                traceTimestampStatistics.setReachedBatchNum(traceTimestampStatistics.getReachedBatchNum() + 1);
+                traceTimestampStatisticsMapper.updateByPrimaryKey(traceTimestampStatistics);
             }
-            traceTimestampStatistics.setTempTimestamp(lastMaxTimestamp);
-            traceTimestampStatistics.setLastEndTimestamp(currentMaxTimestamp);
-            traceTimestampStatistics.setReachedBatchNum(traceTimestampStatistics.getReachedBatchNum() + 1);
-            traceTimestampStatisticsMapper.updateByPrimaryKey(traceTimestampStatistics);
         }
-    }
 
 //    @Async(VarConst.THREAD_POOL)
 //    public void handleGc() {
 //        System.gc();
 //    }
 
+    }
 }
